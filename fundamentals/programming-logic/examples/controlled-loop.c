d20dbf381bc067f3ce113287d678533f26b69b12
Kauã Falcão <86930146+oficial-Kaua-falcao@users.noreply.github.com>
2025-12-30 16:24:07 -0300

    Add controlled-loop.c example for counting

---
+++ fundamentals/programming-logic/examples/controlled-loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int counter = 1;
+
+    while (counter <= 5) {
+        printf("Counter value: %d\n", counter);
+        counter++;
+    }
+
+    return 0;
+}
